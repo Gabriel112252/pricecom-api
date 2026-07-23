d1dd26f8e95c789d9e3657f5dc217264ee8e9e75
GMatiasAlwaysfit <gabriel@alwaysfit.com.br>
2026-07-22 21:34:46 -0300

    fixing a

--- app/controllers/api/v1/tiktok_oauth_controller.rb
+++ app/controllers/api/v1/tiktok_oauth_controller.rb
@@ -16,7 +16,6 @@ module Api
       end
 
       def callback
-        app_key = params[:app_key].to_s.strip
         auth_code = params[:code].to_s.strip
 
         return redirect_error("Callback sem código de autorização") if auth_code.blank?
@@ -26,7 +25,6 @@ module Api
 
         credential = tenant.channel_credentials.find_by(channel: "tiktok")
         return redirect_error(MISSING_CREDENTIALS_MESSAGE) unless tiktok_credentials_configured?(credential)
-        return redirect_error("App Key inválida") unless valid_app_key?(app_key, tiktok_app_key(credential))
 
         token_data = Integrations::TiktokOauthTokenClient
           .new(app_key: tiktok_app_key(credential), app_secret: tiktok_app_secret(credential))
@@ -70,13 +68,6 @@ module Api
 
       def tiktok_callback_url
         "#{request.base_url.sub(/\Ahttp:/, "https:")}/api/v1/webhooks/tiktok"
-      end
-
-      def valid_app_key?(app_key, expected_app_key)
-        return false if app_key.blank? || expected_app_key.blank?
-        return false unless app_key.bytesize == expected_app_key.bytesize
-
-        ActiveSupport::SecurityUtils.secure_compare(app_key, expected_app_key)
       end
 
       def tiktok_credentials_configured?(credential)
